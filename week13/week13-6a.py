ba8e828cb535471393983ab6f3e29e70843707ef
yuhan4688 <12750131@gmail.com>
2023-12-05 11:56:46 +0800

    week13

---
+++ week13/week13-6a.py
@@ -0,0 +1,9 @@
+#soit106_advance_003
+
+a=list(map(int,input().split()))
+
+N=len(a)-1
+
+for i in range(N-1,-1,-1):
+	print(a[i],end=' ')
+print()
---
+++ week13/week13-6b.py
@@ -0,0 +1,8 @@
+#soit106_advance_006
+a=int(input())
+
+ans=0
+for i in range(1,a):
+	ans+=i*(i+1)
+
+print(ans)
