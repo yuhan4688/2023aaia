05b8a15e07c1041485ee5c3592f1372308731faa
yuhan4688 <12750131@gmail.com>
2023-12-19 11:32:39 +0800

    week15

---
+++ week15/week15-5b.py
@@ -0,0 +1,7 @@
+#soit108_advance_009b
+a=input()
+
+for c in a:
+	if c!=' ' and c!='-':
+		print(c)
+		break
---
+++ week15/week15-5c.py
@@ -0,0 +1,8 @@
+#soit_107_advance_005
+a=int(input())
+b=int(input())
+
+if a>b : ans=1
+if a==b: ans=0
+if a<b: ans=-1
+print(ans,end='')
