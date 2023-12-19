2250f0206196e3e21c62bc8bbc8674c009771c9f
yuhan4688 <12750131@gmail.com>
2023-12-19 10:29:23 +0800

    week15

---
+++ week15/week15-2a.py
@@ -0,0 +1,4 @@
+#soit107_advancea-002
+a=list(map(int,input().split()))
+
+print(max(a)-min(a),end='')
---
+++ week15/week15-2b.py
@@ -0,0 +1,8 @@
+#soit107_advance_003
+ans=0
+while True:
+	a=int(input('Enter an integer ( 999 to end ): \n'))
+	if a==999:
+		break
+	ans+= a
+print('The total is:',ans,end='')
