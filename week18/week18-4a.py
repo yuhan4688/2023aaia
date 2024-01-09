40926a489d7475b1dc5a46830b37dc4e03aaccbe
yuhan4688 <12750131@gmail>
2024-01-09 12:01:00 +0800

    week18

---
+++ week18/week18-4a.py
@@ -0,0 +1,10 @@
+#chapter2
+#a=int(input())
+a=int('12345677')
+print(a+999)
+#bool()False Ture
+#int() 0 1 2 100 199 -33
+#float() 3.1415 2.7182 ...
+#str()'單引號' "雙引號"
+str=type('12345')
+print(ans)
